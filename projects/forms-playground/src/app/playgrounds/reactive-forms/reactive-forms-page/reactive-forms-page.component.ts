923f383ff2dc1e12c243a2a49f86b8119de456fd
Caio Rocha <rochacaiodev@gmail.com>
2025-11-20 00:08:36 -0300

    feat: add content until formArray class

--- projects/forms-playground/src/app/playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
+++ projects/forms-playground/src/app/playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
@@ -1,28 +1,57 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './reactive-forms-page.component.html',
   styleUrls: [
     '../../common-page.scss',
     '../../common-form.scss',
-    './reactive-forms-page.component.scss'
+    './reactive-forms-page.component.scss',
   ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ReactiveFormsPageComponent implements OnInit {
-  
+  form = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    nickName: new FormControl(''),
+    email: new FormControl(''),
+    yearOfBirth: new FormControl(''),
+    passport: new FormControl(''),
+    address: new FormGroup({
+      fullAddress: new FormControl(''),
+      city: new FormControl(''),
+      postCode: new FormControl(''),
+    }),
+    phones: new FormArray([new FormControl('')]),
+  });
+
   get years() {
     const now = new Date().getUTCFullYear();
-    return Array(now - (now - 40)).fill('').map((_, idx) => now - idx);
-  }
-  
-  constructor() { }
-
-  ngOnInit(): void {
+    return Array(now - (now - 40))
+      .fill('')
+      .map((_, idx) => now - idx);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  addPhone() {
+    // this.form.controls.phones.push(new FormControl(''));
+    this.form.controls.phones.insert(0, new FormControl(''));
+  }
+
+  removePhone(index: number) {
+    this.form.controls.phones.removeAt(index);
+  }
 }
