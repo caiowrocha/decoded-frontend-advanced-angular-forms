829686c6c0609b9f26672a091b4a59cfcd6915d7
Caio Rocha <rochacaiodev@gmail.com>
2025-11-20 13:32:27 -0300

    feat: standalone formcontrol lesson

--- projects/forms-playground/src/app/core/header/header.component.ts
+++ projects/forms-playground/src/app/core/header/header.component.ts
@@ -1,20 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent implements OnInit {
   search!: string;
+  reactiveSearchString = new FormControl('');
+
   constructor() {}
-
   ngOnInit(): void {}
-
 }
