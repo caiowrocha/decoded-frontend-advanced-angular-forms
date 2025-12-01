580d7aed2d873c767ad5cc2a01b075c1ba205128
Caio Rocha <rochacaiodev@gmail.com>
2025-12-01 18:41:11 -0300

    feat: add skills boiler plate code

--- projects/forms-playground/src/app/playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
+++ projects/forms-playground/src/app/playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
@@ -1,11 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import {
-  FormArray,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-} from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Observable, tap } from 'rxjs';
+import { UserSkillsService } from '../../../core/users-skills.service';
+
 
 @Component({
   selector: 'app-reactive-forms-page',
@@ -15,63 +13,78 @@ import {
   styleUrls: [
     '../../common-page.scss',
     '../../common-form.scss',
-    './reactive-forms-page.component.scss',
+    './reactive-forms-page.component.scss'
   ],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ReactiveFormsPageComponent implements OnInit {
-  phoneLabels = ['Mobile', 'Home', 'Work'];
+
+  phoneLabels = ['Main', 'Mobile', 'Work', 'Home'];
+  years =  this.getYears();
+  skills$!: Observable<string[]>;
 
   form = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    nickName: new FormControl(''),
-    email: new FormControl(''),
-    yearOfBirth: new FormControl(''),
+    firstName: new FormControl('Dmytro'),
+    lastName: new FormControl('Mezhenskyi'),
+    nickname: new FormControl(''),
+    email: new FormControl('dmytro@decodedfrontend.io'),
+    yearOfBirth: new FormControl(this.years[this.years.length - 1], { nonNullable: true }),
     passport: new FormControl(''),
     address: new FormGroup({
-      fullAddress: new FormControl(''),
-      city: new FormControl(''),
-      postCode: new FormControl(''),
+      fullAddress: new FormControl('', { nonNullable: true }),
+      city: new FormControl('', { nonNullable: true }),
+      postCode: new FormControl(0, { nonNullable: true }),
     }),
     phones: new FormArray([
       new FormGroup({
-        phone: new FormControl(''),
-        label: new FormControl(this.phoneLabels[0]),
-      }),
+        label: new FormControl(this.phoneLabels[0], { nonNullable: true }),
+        phone: new FormControl('')
+      })
     ]),
+    skills: new FormGroup<{
+      [key: string]: FormControl<boolean>;
+    }>({})
   });
 
-  get years() {
-    const now = new Date().getUTCFullYear();
-    return Array(now - (now - 40))
-      .fill('')
-      .map((_, idx) => now - idx);
+  constructor(private userSkills: UserSkillsService) { }
+
+  ngOnInit(): void {
+    this.skills$ = this.userSkills
+    .getSkills()
+    .pipe(
+      tap(skills => {
+        this.buildSkillsControls(skills)
+      })
+    )
   }
 
-  constructor() {}
-
-  ngOnInit(): void {}
-
   addPhone() {
-    // this.form.controls.phones.push(new FormGroup({
-    //   phone: new FormControl(''),
-    //   label: new FormControl(''),
-    // }));
-    this.form.controls.phones.insert(
-      0,
+    this.form.controls.phones.insert(0,
       new FormGroup({
-        phone: new FormControl(''),
-        label: new FormControl(this.phoneLabels[0]),
+        label: new FormControl(this.phoneLabels[0], { nonNullable: true }),
+        phone: new FormControl('')
       })
-    );
+    )
   }
 
   removePhone(index: number) {
     this.form.controls.phones.removeAt(index);
   }
 
-  onSubmit(event: Event) {
+  onSubmit(e: Event) {
     console.log(this.form.value);
+  }
+
+  private getYears() {
+    const now = new Date().getUTCFullYear();
+    return Array(now - (now - 40)).fill('').map((_, idx) => now - idx);
+  }
+
+  private buildSkillsControls(skills: string[]) {
+    skills.forEach(skill =>
+      this.form.controls.skills.addControl(skill, new FormControl(false, {
+        nonNullable: true,
+      }))
+    );
   }
 }
