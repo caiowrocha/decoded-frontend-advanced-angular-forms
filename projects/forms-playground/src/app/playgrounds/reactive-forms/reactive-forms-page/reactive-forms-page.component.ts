73e1231976694c8c36e23124f2620f6e8b64bdb0
Caio Rocha <rochacaiodev@gmail.com>
2025-12-02 23:26:25 -0300

    feat: add validators lesson

--- projects/forms-playground/src/app/playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
+++ projects/forms-playground/src/app/playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
@@ -1,9 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Observable, tap } from 'rxjs';
 import { UserSkillsService } from '../../../core/users-skills.service';
-
+import { banWords } from '../validators/ban-words.validator';
+import { passwordShouldMatch } from '../validators/password-should-match.validator';
 
 @Component({
   selector: 'app-reactive-forms-page',
@@ -13,23 +20,43 @@ import { UserSkillsService } from '../../../core/users-skills.service';
   styleUrls: [
     '../../common-page.scss',
     '../../common-form.scss',
-    './reactive-forms-page.component.scss'
+    './reactive-forms-page.component.scss',
   ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ReactiveFormsPageComponent implements OnInit {
-
   phoneLabels = ['Main', 'Mobile', 'Work', 'Home'];
-  years =  this.getYears();
+  years = this.getYears();
   skills$!: Observable<string[]>;
 
   form = this.fb.group({
-    firstName: ['Dmytro', Validators.required, Validators.minLength(3)],
-    lastName: ['Mezhenskyi', [Validators.required, Validators.minLength(3)]],
-    nickname: ['', [Validators.required, Validators.minLength(3), Validators.pattern(/ˆ[\w.]+$/)]],
-    email: ['dmytro@decodedfrontend.io', [Validators.required, Validators.email]],
-    yearOfBirth: this.fb.nonNullable.control(this.years[this.years.length - 1], Validators.required),
-    passport: ['', [Validators.required, Validators.pattern(/^[A-Z]{2}[0-9]{6}$/)]],
+    firstName: [
+      'Dmytro',
+      [
+        Validators.required,
+        Validators.minLength(4),
+        banWords(['test', 'dummy']),
+      ],
+    ],
+    lastName: ['Mezhenskyi', [Validators.required, Validators.minLength(2)]],
+    nickname: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.pattern(/^[\w.]+$/),
+        banWords(['dummy', 'anonymous']),
+      ],
+    ],
+    email: [
+      'dmytro@decodedfrontend.io',
+      [Validators.email, Validators.required],
+    ],
+    yearOfBirth: this.fb.nonNullable.control(
+      this.years[this.years.length - 1],
+      Validators.required
+    ),
+    passport: ['', [Validators.pattern(/^[A-Z]{2}[0-9]{6}$/)]],
     address: this.fb.nonNullable.group({
       fullAddress: ['', Validators.required],
       city: ['', Validators.required],
@@ -38,31 +65,37 @@ export class ReactiveFormsPageComponent implements OnInit {
     phones: this.fb.array([
       this.fb.group({
         label: this.fb.nonNullable.control(this.phoneLabels[0]),
-        phone: ''
-      })
+        phone: '',
+      }),
     ]),
-    skills: this.fb.record<boolean>({})
+    skills: this.fb.record<boolean>({}),
+    password: this.fb.group(
+      {
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: '',
+      },
+      {
+        validators: passwordShouldMatch,
+      }
+    ),
   });
 
-  constructor(private userSkills: UserSkillsService, private fb: FormBuilder) { }
+  constructor(private userSkills: UserSkillsService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.skills$ = this.userSkills
-    .getSkills()
-    .pipe(
-      tap(skills => {
-        this.buildSkillsControls(skills)
-      })
-    )
+      .getSkills()
+      .pipe(tap((skills) => this.buildSkillControls(skills)));
   }
 
   addPhone() {
-    this.form.controls.phones.insert(0,
+    this.form.controls.phones.insert(
+      0,
       new FormGroup({
         label: new FormControl(this.phoneLabels[0], { nonNullable: true }),
-        phone: new FormControl('')
+        phone: new FormControl(''),
       })
-    )
+    );
   }
 
   removePhone(index: number) {
@@ -75,14 +108,17 @@ export class ReactiveFormsPageComponent implements OnInit {
 
   private getYears() {
     const now = new Date().getUTCFullYear();
-    return Array(now - (now - 40)).fill('').map((_, idx) => now - idx);
+    return Array(now - (now - 40))
+      .fill('')
+      .map((_, idx) => now - idx);
   }
 
-  private buildSkillsControls(skills: string[]) {
-    skills.forEach(skill =>
-      this.form.controls.skills.addControl(skill, new FormControl(false, {
-        nonNullable: true,
-      }))
+  private buildSkillControls(skills: string[]) {
+    skills.forEach((skill) =>
+      this.form.controls.skills.addControl(
+        skill,
+        new FormControl(false, { nonNullable: true })
+      )
     );
   }
 }
---
+++ projects/forms-playground/src/app/playgrounds/reactive-forms/validators/ban-words.validator.ts
@@ -0,0 +1,13 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export function banWords(bannedWords: string[] = []): ValidatorFn {
+  return (control: AbstractControl<string | null>): ValidationErrors | null => {
+    const foundBannedWord = bannedWords.find(
+      (word) => word?.toLowerCase() === control?.value?.toLowerCase()
+    );
+
+    const error = { banWords: { bannedWord: foundBannedWord } };
+
+    return foundBannedWord ? error : null;
+  };
+}
